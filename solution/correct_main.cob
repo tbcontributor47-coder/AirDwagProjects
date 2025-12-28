fbefbeda5bc4ddfd4084ebb669809c462a0bb2ff
Manoj-Kumar-Selvaraj <ss.mano1998@gmail.com>
2025-12-29 01:23:17 +0530

    Remove extra solution file

--- solution/correct_main.cob
+++
@@ -1,9 +0,0 @@
-*> Correct COBOL program: prints deterministic message
-       identification division.
-       program-id. MAIN.
-       environment division.
-       data division.
-       procedure division.
-           display "COBOL: Hello, world".
-           goback.
-       end program MAIN.
